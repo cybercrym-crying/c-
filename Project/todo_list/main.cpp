b9a659e10fb977056f440c42392496c81ec96b1f
bero <crymbero@gmail.com>
2026-02-01 17:39:03 +0800

    add quit, and list group

--- Project/todo_list/main.cpp
+++ Project/todo_list/main.cpp
@@ -1,27 +1,18 @@
-
 #include <cctype>
 #include <iostream>
+#include <string>
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
 using namespace std;
-bool checkUserInput(string inputUser) {
-  for (char c : inputUser) {
-    if (!(isdigit(c))) {
-      return false;
-    }
-  }
-  return true;
-}
-void displayTodoList(string todo[], int ukuran) {
-  for (int i = 0; i < ukuran; i++) {
-    if (todo[i] != "0") {
-      cout << i + 1 << ". " << todo[i] << endl;
-      continue;
-    }
-    break;
-  }
-}
-void clearScreen() { std::cout << "\033[2J\033[H"; }
 
+bool checkUserInput(string inputUser);
+void displayTodoList(string todo[], int ukuran);
+void clearScreen();
+void groupDisplay();
 int main() {
+  groupDisplay();
+
   string todoList[101], inputUser;
   int n = sizeof(todoList) / sizeof(todoList[0]);
   for (int i = 0; i < n; i++) {
@@ -30,11 +21,12 @@ int main() {
   while (true) {
     cout << "----- TODO LIST PROGRAM -----\n\n";
     cout << "1. Add Todo List\n";
-    cout << "2. Checked Todo List\n\n";
+    cout << "2. Checked Todo List\n";
+    cout << "3. Quit\n\n";
     cout << "Input Your Choice : ";
     cin >> inputUser;
     cin.ignore();
-    clearScreen();
+
     switch (stoi(inputUser)) {
 
     case 1:
@@ -45,16 +37,18 @@ int main() {
         getline(cin, inputUser);
 
         if (inputUser == "0") {
-          clearScreen();
+
           break;
         }
         todoList[i] = inputUser;
       }
+      cout << endl;
       continue;
     case 2:
       while (true) {
         displayTodoList(todoList, n);
         cout << endl;
+        cout << "[0] To Quit" << endl;
         cout << "Input Your Choice : ";
         cin >> inputUser;
         if (checkUserInput(inputUser) == false || inputUser == "0") {
@@ -63,14 +57,47 @@ int main() {
         }
         todoList[stoi(inputUser) - 1] =
             todoList[stoi(inputUser) - 1] + "     \t[ Done ]";
-        clearScreen();
+
         continue;
       }
+      cout << endl;
+      continue;
+    case 3:
+      cout << "Program Stoped\n\n";
+      exit(0);
     default:
-      clearScreen();
       cout << "Not In Option" << endl;
       continue;
     }
   }
   return 0;
+}
+
+void clearScreen() { std::cout << "\033[2J\033[H"; }
+
+void displayTodoList(string todo[], int ukuran) {
+  for (int i = 0; i < ukuran; i++) {
+    if (todo[i] != "0") {
+      cout << i + 1 << ". " << todo[i] << endl;
+      continue;
+    }
+    break;
+  }
+}
+
+bool checkUserInput(string inputUser) {
+  for (char c : inputUser) {
+    if (!(isdigit(c))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void groupDisplay() {
+  cout << BOLD << GREEN << "-----KELOMPOK 9-----" << RESET << endl;
+  cout << GREEN
+       << "1. Ajis Aditya Alzahril\n2. Akbar Rachim\n3. M. Radja Nur Akbar\n"
+       << RESET;
+  cout << BOLD << GREEN << "--------------------" << RESET << endl << endl;
 }
