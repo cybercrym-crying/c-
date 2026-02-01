7999fc30be7d5f5d484c3b989ba7208b37478e9e
bero <crymbero@gmail.com>
2026-02-01 17:40:24 +0800

    add class, item,tools,character,material

--- Project/simple_mining_game/main.cpp
+++ Project/simple_mining_game/main.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+class Item {
+public:
+  string itemName;
+  Item(string name) : itemName(name) {}
+};
+
+class Material : public Item {
+private:
+  int mountMax = 99;
+
+public:
+  Material(string name, int max) : Item(name), mountMax(max) {}
+};
+
+class
+
+    class Tools : public Item {
+private:
+  int duraMax;
+  int stackMax = 1;
+
+public:
+  Tools(string name, int dura) : Item(name), duraMax(dura) {}
+};
+
+class Character {
+private:
+  string charName;
+  int charEnergy = 10;
+  int charLevel = 1;
+  Tools *tool;
+
+public:
+  Character(string name, Tools *t) : charName(name), tool(t) {}
+  void useTool() { cout << "Using " << tool->itemName << endl; }
+};
+
+int main() {
+  vector<Material> metal;
+  Tools *ironPickaxe = new Tools("Iron Pickaxe", 50);
+
+  Character player("Eliced Star", ironPickaxe);
+  player.useTool();
+
+  return 0;
+}
