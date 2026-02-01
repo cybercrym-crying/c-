588d21c0794e94fa89ca1456e002f737b5556483
bero <crymbero@gmail.com>
2026-02-01 18:07:43 +0800

    add World Object and adding function take metal list from file

--- Project/simple_mining_game/main.cpp
+++ Project/simple_mining_game/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,13 +17,22 @@ private:
 public:
   Material(string name, int max) : Item(name), mountMax(max) {}
 };
+
+class WorldObject : public Item {
+private:
+  int durability;
+
+public:
+  WorldObject(string name, int dura) : Item(name), durability(dura) {}
+};
+
 class Tools : public Item {
 private:
-  int duraMax;
+  int durability;
   int stackMax = 1;
 
 public:
-  Tools(string name, int dura) : Item(name), duraMax(dura) {}
+  Tools(string name, int dura) : Item(name), durability(dura) {}
 };
 
 class Character {
@@ -38,11 +48,33 @@ public:
 };
 
 int main() {
-  vector<Material> metal;
+  vector<WorldObject> metalBlock;
   Tools *ironPickaxe = new Tools("Iron Pickaxe", 50);
 
   Character player("Eliced Star", ironPickaxe);
   player.useTool();
 
   return 0;
+}
+
+void addingMetalList(vector<WorldObject> *metal) {
+  string name, bufferSR;
+  int dura, spawnRate;
+  ifstream fileMetal("metal_list.txt");
+  if (fileMetal.is_open()) {
+    while (fileMetal >> name >> dura >> bufferSR) {
+      if (bufferSR == "COMMON") {
+        spawnRate = COMMON;
+      } else if (bufferSR == "UNCOMMON") {
+        spawnRate = UNCOMMON;
+      } else if (bufferSR == "RARE") {
+        spawnRate = RARE;
+      } else if (bufferSR == "EPIC") {
+        spawnRate = EPIC;
+      } else {
+        spawnRate == LEGENDARY
+      }
+    }
+    fileMetal.close();
+  }
 }
